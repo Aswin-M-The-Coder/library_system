51dcf2ee14a052fae5eac1330c654fd7f48d3750
Aswin M <aswinschool267@gmail.com>
2024-02-22 10:15:31 +0530

    updated-1

--- lLibrary_Backend/server/server.js
+++ lLibrary_Backend/server/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 // Database connection
 const connection = mysql.createConnection({
