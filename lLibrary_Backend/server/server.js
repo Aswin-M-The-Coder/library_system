f6b8f1b45535e2eeb3f92ab581261613a19877ab
Aswin M <aswinschool267@gmail.com>
2024-02-22 10:48:51 +0530

    updated-3

--- lLibrary_Backend/server/server.js
+++ lLibrary_Backend/server/server.js
@@ -70,7 +70,7 @@ app.post('/search', (req, res) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
